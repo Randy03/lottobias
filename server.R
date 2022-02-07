d2ee5a1309b45c126d0e4023a8e938cef0b4d338
Tomas Ramella <tomas.ramella@gmail.com>
2022-02-07 16:23:05 +0000

    added table

--- server.R
+++ server.R
@@ -2,6 +2,7 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
+set.seed(123)
 create_lotto_df <-function(n){
   number <- do.call(paste0, replicate(5, sample(0:9, n, TRUE), FALSE))
   date <- seq(Sys.Date()-(n-1), Sys.Date(), by="days")
@@ -11,6 +12,6 @@ create_lotto_df <-function(n){
 lotto_df <- create_lotto_df(10000)
 
 shinyServer(function(input, output) {
-  output$winnersTable <- renderDataTable(lotto_df %>% arrange(desc(date)) %>% top_n(7))
+  output$winnersTable <- renderTable(lotto_df %>% arrange(desc(date)) %>% slice_head(n=7))
   
 })
--- ui.R
+++ ui.R
@@ -1,11 +1,16 @@
 library(shiny)
 library(shinyjs)
 
-lotto_number <- {
-  numericInput("favNumber",label="What's your lucky favourite number",value=0,min = 0)
-  h4("Last winners")
-  dataTableOutput('winnersTable')
-}
+lotto_number <- fluidRow(
+  column(8,
+         numericInput("favNumber",label="What's your lucky favourite number",value=0,min = 0),
+         h4("Last winners"),
+         tableOutput('winnersTable')     
+  ),
+  column(4,
+         verbatimTextOutput("lottoResult")
+  )
+)
 
 lotto_good_one <- {
   h4("Which one is a real lotto number?")
