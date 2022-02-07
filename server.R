e54c76c45d6ff34b2356dff3a5cde9bc7216edfd
Tomas Ramella <tomas.ramella@gmail.com>
2022-02-07 14:44:16 +0000

    bsae schema

---
+++ server.R
@@ -0,0 +1,11 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+
+
+shinyServer(function(input, output) {
+ 
+  
+  
+})
---
+++ ui.R
@@ -0,0 +1,29 @@
+library(shiny)
+library(shinyjs)
+
+lotto_number <- {
+  numericInput("favNumber",label="What's your favourite number",value=0,min = 0)
+}
+
+lotto_good_one <- {
+  h4("Which one is a real lotto number?")
+  
+}
+
+lotto_bad_one <- {
+  h4("Which number is more likely to win lottery")
+
+}
+
+shinyUI(
+  fluidPage(
+    titlePanel("Lotto bias"),
+    navlistPanel(
+      id = "tabset",
+      "Games",
+      tabPanel("What's your favourite number", lotto_number),
+      tabPanel("Guess the good one", lotto_good_one),
+      tabPanel("Guess the bad one", lotto_bad_one)
+    )
+  )
+)
