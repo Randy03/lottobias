0760c980403b7c3e01a6cb33d4b2aaf2b5fdfb92
abartolomeredon <92445124+abartolomeredon@users.noreply.github.com>
2022-02-07 21:40:30 +0100

    Add files via upload

--- server.R
+++ server.R
@@ -22,32 +22,37 @@ shinyServer(function(input, output) {
   
   store <- reactiveValues()
   
-  store$list <- list(0)
+  store$list <- data.frame("count"=0)
   
   store$num_select <- c(paste(lotto_df %>% sample_n(1) %>% select(number)),
-                    paste(guess_good_df %>% sample_n(1) %>% select(number)),
-                    sample(c(1, 2), 1))
+                    paste(guess_good_df %>% sample_n(1) %>% select(number)))
   
   
-  output$sel_option_1 <- renderText(paste(store$num_select[sample_idx]))
   
-  output$sel_option_2 <- renderText(paste(store$num_select[if(sample_idx==1){2} else{1}]))
+  store$sample_idx <- sample(c(1, 2), 1)
+  
+  output$sel_option_1 <- renderText(paste(store$num_select[store$sample_idx]))
+  
+  output$sel_option_2 <- renderText(paste(store$num_select[if(store$sample_idx==1){2} else{1}]))
   
   
   observeEvent(input$select1,{
     
     if (sample_idx == 1){
       
-      store$list <- append(store$list, 1)
+      store$list <- store$list %>% add_row(count=0)
       
     }
     
-    else{store$list <- append(store$list, 0)}
+    else{store$list <- store$list %>% add_row(count=1)}
     
     
     store$num_select <- c(paste(lotto_df %>% sample_n(1) %>% select(number)),
                       paste(guess_good_df %>% sample_n(1) %>% select(number)),
                       sample(c(1, 2), 1))
+    
+    
+    store$sample_idx <- sample(c(1, 2), 1)
 
     
   })
@@ -57,24 +62,26 @@ shinyServer(function(input, output) {
     
     if (sample_idx != 1){
       
-      store$list <- append(store$list, 1)
+      store$list <- store$list %>% add_row(count=0)
       
     }
     
-    else{store$list <- append(store$list, 0)}
+    else{store$list <- store$list %>% add_row(count=1)}
     
     
     store$num_select <- c(paste(lotto_df %>% sample_n(1) %>% select(number)),
                           paste(guess_good_df %>% sample_n(1) %>% select(number)),
                           sample(c(1, 2), 1))
     
+    store$sample_idx <- sample(c(1, 2), 1)
+    
   })
     
 
    
-    output$guess_right <- renderText(paste("You've found ", sum(store$list), " fake numbers !!"))
+    output$guess_right <- renderText(paste("You've found ", sum(store$list$count), " fake numbers !!"))
     
-    output$total_per <- renderText(paste0(sum(store$list), "/", length(store$list)-1,"\n","(",100*sum(store$list)/(length(store$list)-1), "%)" )) 
+    output$total_per <- renderText(paste0(sum(store$list$count), "/", length(store$list$count)-1,"\n","(",100*sum(store$list$count)/(length(store$list$count)-1), "%)" )) 
     
   
   
